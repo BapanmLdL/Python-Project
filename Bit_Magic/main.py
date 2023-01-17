ddd2bf35429c7e5fe93d330038aed6cc51a42217
BapanmLdL <85588798+BapanmLdL@users.noreply.github.com>
2023-01-17 11:45:02 +0530

    Add files via upload

---
+++ Bit_Magic/main.py
@@ -0,0 +1,32 @@
+# print(bin(18))
+# print(bin(12))
+# print(int('0b10010', 2))
+# print(int('0b1100', 2))
+
+a = 3
+b = 6
+#Bitwise AND
+print(a & b)
+
+#Bitwise OR
+print(a | b)
+
+#Bitwise XOR
+print(a ^ b)
+
+x = 5
+
+#Left Shift
+print(x << 1)
+print(x << 2)
+print(x << 3)
+
+#Right Shift
+print(x >> 1)
+print(x >> 2)
+print(x >> 3)
+print(x >> 4)
+
+#Bitwise Not
+print(~x)
+
